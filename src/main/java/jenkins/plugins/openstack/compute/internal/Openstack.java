2820802f0e4663b61e3e5f2ee30084f88a4d7e68
Ian Duffy <ian@ianduffy.ie>
2015-12-15 07:55:36 +0000

    Update to use the v3 auth builder
    Signed-off-by: Ian Duffy <ian@ianduffy.ie>

--- src/main/java/jenkins/plugins/openstack/compute/internal/Openstack.java
+++ src/main/java/jenkins/plugins/openstack/compute/internal/Openstack.java
@@ -39,6 +39,7 @@ import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
 import org.openstack4j.api.OSClient;
 import org.openstack4j.api.compute.ComputeFloatingIPService;
+import org.openstack4j.model.common.Identifier;
 import org.openstack4j.model.compute.ActionResponse;
 import org.openstack4j.model.compute.Address;
 import org.openstack4j.model.compute.Flavor;
@@ -75,16 +76,11 @@ public class Openstack {
     private final OSClient client;
 
     public Openstack(@Nonnull String endPointUrl, @Nonnull String identity, @Nonnull Secret credential, @CheckForNull String region) {
-        // TODO refactor to split username:tenant everywhere including UI
-        String[] id = identity.split(":", 2);
-        String username = id.length > 0 ? id[0] : "";
-        String tenant = id.length > 1 ? id[1] : "";
-        client = OSFactory.builder().endpoint(endPointUrl)
-                .credentials(username, credential.getPlainText())
-                .tenantName(tenant)
+        client = OSFactory.builderV3().endpoint(endPointUrl)
+                .credentials(identity, credential.getPlainText(), Identifier.byName("AD"))
+                .scopeToProject(Identifier.byName("de"), Identifier.byName("AD"))
                 .authenticate()
-                .useRegion(region)
-        ;
+                .useRegion(region);
     }
 
     public @Nonnull List<? extends Network> getSortedNetworks() {
